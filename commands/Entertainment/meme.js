94c25033e62747b2705f4595a885f7a66cea935f
Corbinator333 <corbinator@live.com>
2022-12-13 18:40:25 -0800

    Update Command - Disabled Meme

--- commands/Entertainment/meme.js
+++ commands/Entertainment/meme.js
@@ -5,12 +5,13 @@ module.exports = {
   description: "Fetch a meme!",
   data: new SlashCommandBuilder()
         .setName('meme')
-        .setDescription('Fetch a meme')
+        .setDescription('This command is temporarily disabled.')
         .setDMPermission(false),
 
   run: async (client, interaction) => {
         const rawAPI = await fetch('https://meme-api.herokuapp.com/gimme');
         const decryptedAPI = await rawAPI.json();
-        interaction.reply({ content: `${decryptedAPI.url}`, ephemeral: true });
+        //interaction.reply({ content: `${decryptedAPI.url}`, ephemeral: true });
+        interaction.reply({ content: `This command is temporarily disabled.`, ephemeral: true });
   },
 };
