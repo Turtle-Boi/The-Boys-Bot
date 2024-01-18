2f12c498a8e90c42d37865390bfce1a328e57354
Turtle <corbinator@live.com>
2024-01-17 22:10:53 -0800

    Update ready.js
    VC logs channel on ready msg

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,9 @@ client.on("ready", () => {
   const calculatedTimestamp = Math.floor(generatedTimestamp / 1000)
   console.log(`${client.user.tag} is up and ready to go!`);
   const logChannel = client.channels.cache.get("1028903092684591154")
+  const vcLogs = client.channels.cache.get("1044739015640891543")
   logChannel.send(`**Bot online at** <t:${calculatedTimestamp}:T>**.**`)
+  vcLogs.send(`Bot rebooted, some join logs may be omitted.`)
   function setStatus() {
     client.user.setActivity(`at ${client.ws.ping} ping`, { type: ActivityType.Playing })
   }
